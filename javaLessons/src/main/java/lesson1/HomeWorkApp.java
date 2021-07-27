7f9dd5b19f46aa8a14753fadf1776ebe335e7d54
anduser <churovaa@yandex.by>
2021-07-27 16:25:48 +0300

    1 commit

---
+++ javaLessons/src/main/java/lesson1/HomeWorkApp.java
@@ -0,0 +1,7 @@
+package lesson1;
+
+public class HomeWorkApp {
+    public static void printThreeWords(String[] args) {
+        System.out.println("Hallo2");
+    }
+}
--- javaLessons/src/main/java/lesson1/MainClass.java
+++ javaLessons/src/main/java/lesson1/MainClass.java
@@ -1,4 +1,56 @@
 package lesson1;
 
 public class MainClass {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+
+    public static void printThreeWords() {
+        System.out.println("Orange \nBanana \nApple");
+    }
+
+
+    public static void checkSumSign() {
+        int a = 0;
+        int b = -6;
+        int c = a+b;
+        if (c>=0){
+            System.out.println("Сумма положительная");
+        }
+        else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+
+    public static void printColor () {
+    int value = 665;
+    if (value <=0) {
+            System.out.println("Красный");
+        }
+    if (value >0 && value <=100) {
+            System.out.println("Желтый");
+        }
+    if (value >100) {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 9;
+        int b = 8;
+        if (a>=b){
+            System.out.println("a >= b");
+        }
+        else{
+            System.out.println("a < b");
+        }
+
+    }
+
+
 }
