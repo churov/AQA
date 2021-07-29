c77970fad73a39f60313eb498888844eea81e88e
Artsiom Churau <churovaa@yandex.by>
2021-07-29 15:42:43 +0300

    дз3 start

---
+++ javaLessons/src/main/java/lesson1/Сотрудник.java
@@ -0,0 +1,20 @@
+package src.main.java.lesson1;
+
+public class Сотрудник {
+    public void foo(){
+
+    }
+    private String fio;
+    private String post;
+
+
+    public Сотрудник(String fio, String post, String email, String phone, int salary, int age) {
+        System.out.printf("Конструктор класса Сотрудник");
+        fio = "Мячеслав";
+        post = "Директор";
+    }
+}
+
+
+
+
